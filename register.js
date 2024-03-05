5232900a279b29aea8c257ea5dbde217002fedf7
kimoochie <kkventura.wup@gmail.com>
2024-03-05 14:39:12 +0800

    Added register.js file

--- register.js
+++ register.js
@@ -1,11 +1,26 @@
-let txtUsename = document.querySelector("#txtUsername");
-            let btnRegister = document.querySelector("#btnRegister");
+let txtUsername = document.querySelector("#txtUsername");
+let txPassword = document.querySelector("#TxtPassword");
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
+let btnRegister = document.querySelector("#btnRegister");
         
-            btnRegister.onclick = function () {
-                register(txtUsename.value)
-            }
-            //variables
+btnRegister.onclick = function () {
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value)
+}
+//variables
 
-            function register (username) {
-                console.log(username)
-            }
+function register (username, password, confirmPassword) {
+    // if..else If..else
+    // Conditional Operators (Greater Than >, Less Than <, Equal ==, Not !, 
+    // Greater Than or Equal >=, Less Than or Equal <=
+    // Not Equal !=
+
+    console.log(username, password, confirmPassword)
+
+    if(password == confirmPassword && password != "" && confirmPassword != ""){
+        return console.log("Password match!")
+    } else if (password == "" || confirmPassword == ""){
+         return console.log("Password must not be empty.")
+    } else {
+        return console.log("Password does not match.")
+    }
+}
